af4741e1d2cb148f2ee8a312a8187dffecf3f48e
One <imone@tuta.io>
2021-08-05 17:13:59 +0800

    random algorithm

---
+++ onerl/tests/common/random_algorithm.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+from onerl.utils.batch.cuda import BatchCuda
+
+
+class RandomAlgorithm(nn.Module):
+    def __init__(self,
+                 network: dict,
+                 env_params: dict):
+        super().__init__()
+        self.env_params = env_params
+
+    def forward(self, obs: torch.Tensor):
+        if "act_n" in self.env_params:
+            return torch.randint(0, self.env_params["act_n"], (obs.shape[0], ))
+        else:
+            # uniform -act_max ... act_max
+            return self.env_params["act_max"] * (torch.rand(obs.shape[0], *self.env_params["act_shape"]) * 2 - 1)
+
+    def learn(self, batch: BatchCuda):
+        pass
