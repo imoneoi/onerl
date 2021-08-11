95040debafae20c8451b78a7f616a8c6cc4c8ea2
one <imone@tuta.io>
2021-08-11 17:02:03 +0800

    cuda batch dtype fix

--- onerl/utils/batch/cuda.py
+++ onerl/utils/batch/cuda.py
@@ -1,12 +1,13 @@
 import torch
 
 from onerl.utils.batch.shared import BatchShared
+from onerl.utils.dtype import numpy_to_torch_dtype_dict
 
 
 class BatchCuda:
     def __init__(self, batch_shared: BatchShared, device=None):
         self.batch_shared = batch_shared
-        self.data = {k: torch.zeros(v.shape, dtype=v.dtype, device=device)
+        self.data = {k: torch.zeros(v.shape, dtype=numpy_to_torch_dtype_dict[v.dtype], device=device)
                      for k, v in self.batch_shared.data.items()}
 
     def copy_from(self):
--- onerl/utils/dtype.py
+++ onerl/utils/dtype.py
@@ -3,15 +3,17 @@ import torch
 
 
 numpy_to_torch_dtype_dict = {
-    np.dtype(np.bool): torch.bool,
-    np.dtype(np.uint8): torch.uint8,
-    np.dtype(np.int8): torch.int8,
-    np.dtype(np.int16): torch.int16,
-    np.dtype(np.int32): torch.int32,
-    np.dtype(np.int64): torch.int64,
-    np.dtype(np.float16): torch.float16,
-    np.dtype(np.float32): torch.float32,
-    np.dtype(np.float64): torch.float64,
-    np.dtype(np.complex64): torch.complex64,
-    np.dtype(np.complex128): torch.complex128
+    np.bool_: torch.bool,
+    np.uint8: torch.uint8,
+    np.int8: torch.int8,
+    np.int16: torch.int16,
+    np.int32: torch.int32,
+    np.int64: torch.int64,
+    np.float16: torch.float16,
+    np.float32: torch.float32,
+    np.float64: torch.float64,
+    np.complex64: torch.complex64,
+    np.complex128: torch.complex128
 }
+
+numpy_to_torch_dtype_dict.update({np.dtype(k): v for k, v in numpy_to_torch_dtype_dict.items()})
