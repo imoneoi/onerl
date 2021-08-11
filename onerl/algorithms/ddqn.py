3961ce4c84d0f456c07fbb071e073db55ec472fb
thkkk <282981502@qq.com>
2021-08-11 11:06:46 +0800

    Add some TODO comments

--- onerl/algorithms/ddqn.py
+++ onerl/algorithms/ddqn.py
@@ -49,8 +49,10 @@ class DDQNAlgorithm(Algorithm):
 
     def learn(self, batch: BatchCuda):
         # TODO: WARNING: DistributedDataParallel enabled here
-        # next q
+        # TODO: prioritized replay
         # FIXME: target update
+        # next q
+
         if self.target_iter % self.target_update_freq == 0:
             self.target_network = {k: deepcopy(v) for k, v in self.network.items()}
 
