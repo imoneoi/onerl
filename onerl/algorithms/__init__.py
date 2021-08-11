f06304a6e5ccbd22e3d432ef9e812bdb1e6f5e3b
one <imone@tuta.io>
2021-08-11 17:02:23 +0800

    algorithm __init__.py

---
+++ onerl/algorithms/__init__.py
@@ -0,0 +1,14 @@
+from onerl.algorithms.algorithm import Algorithm
+
+from onerl.algorithms.random import RandomAlgorithm
+
+from onerl.algorithms.ddqn import DDQNAlgorithm
+
+
+__all__ = [
+    "Algorithm",
+
+    "RandomAlgorithm",
+
+    "DDQNAlgorithm"
+]
