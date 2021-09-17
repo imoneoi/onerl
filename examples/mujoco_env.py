f60b73800c90bc13dcf4dd6b20eb27a9f4f66707
One <imone@tuta.io>
2021-09-17 21:46:38 +0800

    Mujoco env action scaling

--- examples/mujoco_env.py
+++ examples/mujoco_env.py
@@ -21,10 +21,25 @@ class SleepEnv(gym.Wrapper):
         return self.env.step(action)
 
 
+class ActionScalingWrapper(gym.ActionWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.scaling = np.mean(env.action_space.high)
+        print("Action scaling wrapper, with factor {}".format(self.scaling))
+
+    def action(self, action):
+        return self.scaling * action
+
+    def reverse_action(self, action):
+        pass
+
+
 def create_mujoco_env(name: str, sleep_time: float = None):
     env = gym.make(name)
     env.observation_space.dtype = np.float32  # override fp32 observation
 
     if sleep_time is not None:
         env = SleepEnv(env, sleep_time)
+    if not np.isclose(env.action_space.high, 1).all():
+        env = ActionScalingWrapper(env)
     return env
