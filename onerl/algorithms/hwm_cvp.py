1123d94825fed581ddb1a317efd33c7eb2eb5ae4
one <imone@tuta.io>
2022-05-09 14:44:58 +0800

    hwm cvp bugfix

--- onerl/algorithms/hwm_cvp.py
+++ onerl/algorithms/hwm_cvp.py
@@ -13,13 +13,13 @@ class HWMCVPAlgorithm(Algorithm):
                  env_params: dict,
                  # Exploration
                  exploration_len: int = 0,
-                 noise_scale: float = 0.2,
+                 noise_scale: float = 0.1,
                  # WM
-                 unroll_steps: int = 5,
+                 unroll_steps: int = 10,
                  gamma: float = 0.99,
                  # learning rates
                  lr_wm: float = 0.001,
-                 lr_policy: float = 0.0003,
+                 lr_policy: float = 0.0001,
                  # unused
                  batch_size: Optional[int] = None,
                  replay_buffer_size: Optional[int] = None
@@ -38,8 +38,10 @@ class HWMCVPAlgorithm(Algorithm):
         self.policy_optimizer = torch.optim.Adam(self.network["P"].parameters(), lr=lr_policy)
 
     def policy_state_dict(self):
+        encoder = self.network["E"]
         policy = self.network["P"]
-        return policy.module.state_dict() if hasattr(policy, "module") else policy.state_dict()
+        return encoder.module.state_dict() if hasattr(encoder, "module") else encoder.state_dict() + \
+               policy.module.state_dict() if hasattr(policy, "module") else policy.state_dict()
 
     def forward(self, obs: torch.Tensor, ticks: int):
         # full exploration
@@ -57,7 +59,7 @@ class HWMCVPAlgorithm(Algorithm):
     def learn(self, batch: BatchCuda):
         with torch.no_grad():
             act = torch.transpose(batch.data["act"], 0, 1)
-            rew = torch.transpose(batch.data["rew"], 0, 1)
+            cum_rew = torch.cumsum(torch.transpose(batch.data["rew"], 0, 1), dim=0)
             next_obs = torch.transpose(batch.data["obs"], 0, 1)[1:]
             # --> FS * N * (*)
 
@@ -65,6 +67,9 @@ class HWMCVPAlgorithm(Algorithm):
             feature_next_obs = feature_next_obs.view(next_obs.shape[0], next_obs.shape[1], *feature_next_obs.shape[1:])
 
         # update WM
+        tot_v_loss = 0
+        tot_c_loss = 0
+
         wm_loss = 0
         h = self.network["E"](batch.data["obs"][:, 0])
         h0 = h.detach()
@@ -72,23 +77,25 @@ class HWMCVPAlgorithm(Algorithm):
             h = self.network["T"](h, act[t])
             v = self.network["V"](h).squeeze(-1)
 
-            loss_v = torch.mean((v - rew[t]) ** 2)
+            loss_v = 0.01 * torch.mean((v - cum_rew[t]) ** 2)
             loss_c = torch.mean(1 - torch.cosine_similarity(h, feature_next_obs[t], dim=-1))
             wm_loss = wm_loss + loss_v + loss_c
+
+            tot_v_loss += loss_v.item()
+            tot_c_loss += loss_c.item()
 
         self.wm_optimizer.zero_grad()
         wm_loss.backward()
         self.wm_optimizer.step()
 
         # update Policy
-        p_loss = 0
         h = h0
         for t in range(self.unroll_steps):
             a = torch.tanh(self.network["P"](h))
             h = self.network["T"](h, a)
-            v = self.network["V"](h).squeeze(-1)
 
-            p_loss = p_loss - torch.mean(v)
+        v = self.network["V"](h).squeeze(-1)
+        p_loss = -torch.mean(v)
 
         self.policy_optimizer.zero_grad()
         p_loss.backward()
@@ -96,5 +103,9 @@ class HWMCVPAlgorithm(Algorithm):
 
         return {
             "wm_loss": wm_loss.item(),
+
+            "v_loss": tot_v_loss,
+            "c_loss": tot_c_loss,
+
             "p_loss": p_loss.item()
         }
